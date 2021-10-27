9c39150696150b2b4be35bdd4353ca45bbac119b
KaziShohiduzzaman <kazishohiduzzaman786@gmail.com>
2021-10-28 03:28:29 +0600

    header done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="ms-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="/home">
+                                <Link to="/home">Home</Link>
+                            </Nav.Link>
+                            <Nav.Link href="/events">
+                                <Link to="/events">Events</Link>
+                            </Nav.Link>
+                            <Nav.Link href="/register">
+                                <button className='btn btn-outline-primary'>Register</button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
